fc246813303059f9aab3bd4d6e065bf5d85ca8f6
Sumanth <sumanthchunduri123@gmail.com>
2022-11-03 17:47:01 +0530

    first commit

---
+++ cmd/api/movies.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"GreenLight/internal/data"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	fmt.Fprintf(w, "%+v\n", input)
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "details of movie %d\n", id)
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "RRR",
+		Runtime:   102,
+		Genres:    []string{"Drama", "History"},
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.logger.Println(err)
+		app.serverErrorResponse(w, r, err)
+		//http.Error(w, "the server encountered a problem", http.StatusInternalServerError)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module GreenLight
+
+go 1.19
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
